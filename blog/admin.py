bd7e40159730df1ed51f047e8d1e099081cca894
agostina <agostinabaglivo@gmail.com>
2019-05-18 18:13:14 +0200

    Added views to create/edit blog post inside the site.

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_date', 'published_date', 'text')
+
 
 # Register your models here.
